52adb552cdc8ab05f49ab4f4ce1e15da51ca594b
spezian <dacjan@mailbox.org>
2026-02-10 01:09:41 +0100

    Add first dialog

--- lib/views/scan_prm_screen.dart
+++ lib/views/scan_prm_screen.dart
@@ -6,6 +6,7 @@ import 'package:ar_flutter_plugin_plus/managers/ar_session_manager.dart';
 import 'package:ar_flutter_plugin_plus/models/ar_node.dart';
 import 'package:flutter/material.dart';
 import 'package:ar_flutter_plugin_plus/ar_flutter_plugin.dart';
+import 'package:magenta_wifi_vision/widgets/magenta_buttons.dart';
 import 'package:magenta_wifi_vision/widgets/scan_button.dart';
 import 'package:vector_math/vector_math_64.dart' hide Colors;
 
@@ -92,7 +93,39 @@ class _ScanPRMScreenState extends State<ScanPRMScreen> {
                   Padding(
                     padding: const .only(bottom: 128.0),
                     child: ScanButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: Text("Room Complete", textAlign: .center,),
+                                  content: Text("Do you want to finish the scan?", textAlign: .center,),
+                                  contentPadding: .only(top: 2.0, bottom: 12.0),
+                                  actionsAlignment: .center,
+                                  actions: [
+                                    Row(
+                                      spacing: 8.0,
+                                      children: [
+                                        Expanded(
+                                          child: MagentaSmallButton(
+                                            onPressed: () {},
+                                            child: Text("Yes"),
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: MagentaSmallButton(
+                                            onPressed: () => Navigator.of(context).maybePop(),
+                                            color: .black,
+                                            child: Text("No"),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                );
+                              }
+                          );
+                        },
                         padding: .all(24.0),
                         icon: Icon(Icons.crop_square_rounded, size: 62.0,)
                     ),
--- lib/widgets/magenta_buttons.dart
+++ lib/widgets/magenta_buttons.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:magenta_wifi_vision/theme.dart';
+enum MagentaSmallButtonColor { magenta, black }
 
 class MagentaSmallButton extends StatelessWidget {
   final VoidCallback? onPressed;
   final bool border;
+  final MagentaSmallButtonColor color;
   final Widget? child;
-  const MagentaSmallButton({super.key, this.onPressed, this.child, this.border = false});
+  const MagentaSmallButton({super.key, this.onPressed, this.border = false, this.color = .magenta, this.child, });
 
   @override
   Widget build(BuildContext context) {
     return FilledButton(
         style: ButtonStyle(
+          backgroundColor: WidgetStatePropertyAll<Color?>(color == .magenta ? magentaColour : Colors.black),
           textStyle: WidgetStatePropertyAll<TextStyle?>(TextStyle(fontSize: 22.0)),
           shape: WidgetStatePropertyAll<OutlinedBorder?>(
               RoundedRectangleBorder(
